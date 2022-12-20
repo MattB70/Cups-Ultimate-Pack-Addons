882859c8326f482b5f7f96bf3369a1bf45fa0505
Matt Borle (Cup) <mattrb@telus.net>
2022-12-20 15:35:57 -0800

    Attempting to make geckolib item anims work

--- src/main/java/com/mattborle/cupsaddons/animation/UseSpecialItem.java
+++ src/main/java/com/mattborle/cupsaddons/animation/UseSpecialItem.java
@@ -10,6 +10,8 @@ import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -23,31 +25,37 @@ import java.util.Objects;
 public class UseSpecialItem {
 
     @SubscribeEvent
-    public static void onSpecialItemUsed(PlayerInteractEvent.RightClickItem event){
-        var player = Minecraft.getInstance().player;
-        if (player == null) return;
+    public static void onSpecialItemUsed(PlayerInteractEvent.RightClickItem event) {
+        // Run on client side, and only once using the MainHand. 0 = true in this comparison for some reason.
+        if (event.getWorld().isClientSide && event.getHand().compareTo(InteractionHand.MAIN_HAND) == 0){
+            var player = Minecraft.getInstance().player;
+            if (player == null) return;
 
-        // Get the animations for the player, if this fails, print an error and return without crashing.
-        var animation = (ModifierLayer<IAnimation>) PlayerAnimationAccess.getPlayerAssociatedData((AbstractClientPlayer) player)
-                .get(new ResourceLocation(CupsAddons.MOD_ID, "animation"));
-        if (animation == null) return;
-
-
-        // Animation selection =========================================================================================
-        // Here we pair every special item in a hash map with another item it can be fueled with.
-        //      Example: Chrysophilist's Pickaxe can be fueled with Lapis Lazuli.
-        // Any item in this list will play the "animation.model.tool.use" animation upon right-clicking.
+            // Get the animations for the player, if this fails, print an error and return without crashing.
+            var animation = (ModifierLayer<IAnimation>) PlayerAnimationAccess.getPlayerAssociatedData((AbstractClientPlayer) player)
+                    .get(new ResourceLocation(CupsAddons.MOD_ID, "animation"));
+            if (animation == null) return;
 
 
-        // Item the player is holding upon right-clicking
-        Item heldItem = event.getItemStack().getItem();
+            // Animation selection =========================================================================================
+            // Here we pair every special item in a hash map with another item it can be fueled with.
+            //      Example: Chrysophilist's Pickaxe can be fueled with Lapis Lazuli.
+            // Any item in this list will play the "animation.model.tool.use" animation upon right-clicking.
 
-        CupsAddons.LOGGER.info("Item1: "+ heldItem +", Item2: "+CupsAddonsCommonConfigs.FUEL_CHRYSOPHILISTS_PICKAXE.get());
-        // check if the player is holding both special item and its fuel, if so, play the animation.
-        if (heldItem.toString() == CupsAddonsCommonConfigs.FUEL_CHRYSOPHILISTS_PICKAXE.get()) {
-            // Get tool use animation (grab tool with two hands in front of the player)
-            animation.setAnimation(new KeyframeAnimationPlayer(Objects.requireNonNull(PlayerAnimationRegistry
-                    .getAnimation(new ResourceLocation("cupsaddons", "animation.model.tool.use")))));
+
+            // Items the player is holding upon right-clicking
+            Item mainHandItem = event.getItemStack().getItem();
+            Item offHandItem = player.getOffhandItem().getItem();
+            CupsAddons.LOGGER.info("mainHandItem: " + mainHandItem + ", offHandItem: " + offHandItem + ", Required Fuel: " + CupsAddonsCommonConfigs.FUEL_CHRYSOPHILISTS_PICKAXE.get());
+
+            // This is the logic for the animation, and as such can exist on the client. The effect is handled by the item.
+            // check if the player is holding both special item and its fuel, if so, play the animation.
+            if (mainHandItem.toString() == "chrysophilists_pickaxe" && offHandItem.toString() == CupsAddonsCommonConfigs.FUEL_CHRYSOPHILISTS_PICKAXE.get()) {
+
+                // Get tool use animation (grab tool with two hands in front of the player)
+                animation.setAnimation(new KeyframeAnimationPlayer(Objects.requireNonNull(PlayerAnimationRegistry
+                        .getAnimation(new ResourceLocation("cupsaddons", "animation.model.tool.use")))));
+            }
         }
     }
 }
--- src/main/java/com/mattborle/cupsaddons/item/ChrysophilistsPickaxeItem.java
+++ src/main/java/com/mattborle/cupsaddons/item/ChrysophilistsPickaxeItem.java
@@ -13,7 +13,6 @@ import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.resources.sounds.Sound;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
@@ -23,7 +22,6 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.client.IItemRenderProperties;
 import net.minecraftforge.network.PacketDistributor;
-import org.apache.commons.lang3.text.WordUtils;
 import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -52,7 +50,7 @@ public class ChrysophilistsPickaxeItem extends PickaxeItem implements IAnimatabl
         to the player through modpack defined methods. These items may not always be totally "unique", but that can
         never be discovered outside completing the task required by the modpack designer.
     */
-
+/*
     private static final int ANIM_USE = 0;
 
     public AnimationFactory factory = GeckoLibUtil.createFactory(this);
@@ -174,4 +172,116 @@ public class ChrysophilistsPickaxeItem extends PickaxeItem implements IAnimatabl
     public AnimationFactory getFactory() {
         return this.factory;
     }
+
+    */
+    public String idleAnimationName = "animation.chrysophilists_pickaxe.idle";
+    public String useAnimationName = "animation.chrysophilists_pickaxe.use";
+    public String activeAnimationName = "animation.chrysophilists_pickaxe.active";
+
+    private static final String CONTROLLER_NAME = "popupController";
+    private static final int ANIM_OPEN = 0;
+    public AnimationFactory factory = GeckoLibUtil.createFactory(this);
+
+
+    public ChrysophilistsPickaxeItem(Tier tier, int p_42962_, float p_42963_, Properties properties) {
+        super(tier, p_42962_, p_42963_, properties
+                .tab(ItemRegistry.CreativeTab.instance)     // Show in creative menu tab
+                .rarity(Rarity.EPIC)                        // Rarity color for title and loot beams
+                .fireResistant()                            // Cannot be destroyed in lava
+                .durability(-1)                     // Durability of -1 means unbreakable
+        );
+        GeckoLibNetwork.registerSyncable(this);
+    }
+
+    @Override
+    public void initializeClient(Consumer<IItemRenderProperties> consumer) {
+        super.initializeClient(consumer);
+        consumer.accept(new IItemRenderProperties() {
+            private final BlockEntityWithoutLevelRenderer renderer = new ChrysophilistsPickaxeRenderer();
+
+            @Override
+            public BlockEntityWithoutLevelRenderer getItemStackRenderer() {
+                return renderer;
+            }
+        });
+    }
+
+    private <P extends IAnimatable> PlayState predicate(AnimationEvent<P> event) {
+        // Set an animation here, even though it won't play due to playstate set to STOP initially. This will stop a null
+        // reference crash. GeckoLib items are a bit janky, but what isn't? :)
+        event.getController().setAnimation(new AnimationBuilder().addAnimation(useAnimationName, ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+        return PlayState.STOP;
+    }
+    private <E extends IAnimatable> PlayState idleAnimation(AnimationEvent<E> event) {
+        event.getController().setAnimation(new AnimationBuilder().addAnimation(idleAnimationName, ILoopType.EDefaultLoopTypes.LOOP));
+        return PlayState.CONTINUE;
+    }
+
+    @Override
+    public void registerControllers(AnimationData data) {
+        AnimationController controller = new AnimationController(this, CONTROLLER_NAME, 20, this::predicate);
+        // Registering a sound listener just makes it so when any sound keyframe is hit
+        // the method will be called.
+        // To register a particle listener or custom event listener you do the exact
+        // same thing, just with registerParticleListener and
+        // registerCustomInstructionListener, respectively.
+        controller.registerSoundListener(this::soundListener);
+        data.addAnimationController(controller);
+        // Idle animation controller
+        data.addAnimationController(new AnimationController(this, "idle", 20, this::idleAnimation));
+    }
+
+    private <ENTITY extends IAnimatable> void soundListener(SoundKeyframeEvent<ENTITY> event) {
+        // The animation for the JackInTheBoxItem has a sound keyframe at time 0:00.
+        // As soon as that keyframe gets hit this method fires and it starts playing the
+        // sound to the current player.
+        // The music is synced with the animation so the box opens as soon as the music
+        // plays the box opening sound
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player != null) {
+            player.playSound(SoundEvents.TOTEM_USE, 1, 1);
+        }
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return this.factory;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        if (!world.isClientSide) {
+            // Gets the item that the player is holding, should be a JackInTheBoxItem
+            final ItemStack stack = player.getItemInHand(hand);
+            final int id = GeckoLibUtil.guaranteeIDForStack(stack, (ServerLevel) world);
+            // Tell all nearby clients to trigger this JackInTheBoxItem
+            final PacketDistributor.PacketTarget target = PacketDistributor.TRACKING_ENTITY_AND_SELF.with(() -> player);
+            GeckoLibNetwork.syncAnimation(target, this, id, ANIM_OPEN);
+        }
+        return super.use(world, player, hand);
+    }
+
+    @Override
+    public void onAnimationSync(int id, int state) {
+        if (state == ANIM_OPEN) {
+            // Always use GeckoLibUtil to get AnimationControllers when you don't have
+            // access to an AnimationEvent
+            final AnimationController controller = GeckoLibUtil.getControllerForID(this.factory, id, CONTROLLER_NAME);
+
+            if (controller.getAnimationState() == AnimationState.Stopped) {
+                final LocalPlayer player = Minecraft.getInstance().player;
+                if (player != null) {
+                    player.displayClientMessage(new TextComponent("Activating Item").withStyle(ChatFormatting.BLUE,ChatFormatting.ITALIC), true);
+                }
+                // If you don't do this, the popup animation will only play once because the
+                // animation will be cached.
+                controller.markNeedsReload();
+                // Set the animation to open the JackInTheBoxItem which will start playing music
+                // and
+                // eventually do the actual animation. Also sets it to not loop
+                controller.setAnimation(new AnimationBuilder().addAnimation(useAnimationName, ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+            }
+        }
+    }
 }
+
