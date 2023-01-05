1a92d2d870e1da29d7720d5a4e5d8c383bae80dc
Matt Borle (Cup) <mattrb@telus.net>
2023-01-04 17:03:30 -0800

    Trying to figure out curios api

--- src/main/java/com/mattborle/cupsaddons/handlers/curios/FerromagneticObjectPassiveHandler.java
+++ src/main/java/com/mattborle/cupsaddons/handlers/curios/FerromagneticObjectPassiveHandler.java
@@ -1,32 +1,33 @@
 package com.mattborle.cupsaddons.handlers.curios;
 
-import net.minecraft.world.entity.Entity;
+import com.mattborle.cupsaddons.CupsAddons;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.util.NonNullSupplier;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import org.jetbrains.annotations.NotNull;
+import top.theillusivec4.curios.api.CuriosApi;
 
-import javax.annotation.Nullable;
 
 @Mod.EventBusSubscriber
 public class FerromagneticObjectPassiveHandler {
 	@SubscribeEvent
 	public static void onEntityAttacked(LivingAttackEvent event) {
-		if (event != null && event.getEntity() != null) {
-			execute(event, event.getEntity());
-		}
-	}
-
-	public static void execute(Entity entity) {
-		execute(null, entity);
-	}
-
-	private static void execute(@Nullable Event event, Entity entity) {
-		if (entity == null)
-			return;
-		if (entity instanceof Player) {
-			// Do something.
+		// If the event involves a living entity
+		if (event != null && event.getEntityLiving() != null) {
+			// If the living entity is a player
+			if (event.getEntityLiving() instanceof Player) {
+				// Only on the server side:
+				if (!event.getEntityLiving().level.isClientSide) {
+					// Do something:
+					CupsAddons.LOGGER.info("");
+					CupsAddons.LOGGER.info("Cups Addons Curios Debug:");
+					CupsAddons.LOGGER.info("   " + CuriosApi.getCuriosHelper().getEquippedCurios(event.getEntityLiving()));
+					CupsAddons.LOGGER.info("");
+				}
+			}
 		}
 	}
 }
