0f8aa44a3c3d82a760d3f445c1086994b399b054
Matt Borle (Cup) <mattrb@telus.net>
2023-05-17 21:55:53 -0700

    Update ExpertsPickaxeItem.java

--- src/main/java/com/mattborle/cupsaddons/item/tool/ExpertsPickaxeItem.java
+++ src/main/java/com/mattborle/cupsaddons/item/tool/ExpertsPickaxeItem.java
@@ -1,17 +1,23 @@
 package com.mattborle.cupsaddons.item.tool;
 
+import com.mattborle.cupsaddons.CupsAddons;
 import com.mattborle.cupsaddons.client.renderer.item.ExpertsPickaxeRenderer;
 import com.mattborle.cupsaddons.config.CupsAddonsCommonConfigs;
 import com.mattborle.cupsaddons.init.ItemRegistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.IItemRenderProperties;
 import org.apache.commons.lang3.text.WordUtils;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -26,8 +32,6 @@ import java.util.function.Consumer;
 public class ExpertsPickaxeItem extends PickaxeItem implements IAnimatable {
 
     public AnimationFactory factory = GeckoLibUtil.createFactory(this);
-    public String playerName = "undefined"; // Name of the first player to receive the item. Placeholder declared.
-    public String timesUsed = "0";          // Number of times the item has been used. Placeholder declared.
 
 
     // Constructor and Usage ===========================================================================================
@@ -42,14 +46,58 @@ public class ExpertsPickaxeItem extends PickaxeItem implements IAnimatable {
     }
     @Override
     public void appendHoverText(ItemStack stack, Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
-        tooltip.add(new TextComponent("Awarded to "+" "+playerName+"."));
-        tooltip.add(new TextComponent("Used "+" "+timesUsed+" times."));
+        int uses = 0;
+        String playerName = "";
+        if(stack.getTag() != null){
+            // Get uses nbt and put it in uses variable if exists
+            if(stack.getTag().get("cupsaddons.uses") != null) {
+                if (stack.getTag().get("cupsaddons.uses").getAsString() != null) {
+                    uses = Integer.parseInt(stack.getTag().get("cupsaddons.uses").getAsString());
+                }
+            }
+            // Get playerName nbt and put it in playerName variable if exists
+            if(stack.getTag().get("cupsaddons.playerName") != null) {
+                if (stack.getTag().get("cupsaddons.playerName").getAsString() != null) {
+                    playerName = stack.getTag().get("cupsaddons.playerName").getAsString();
+                }
+            }
+        }
+        tooltip.add(new TextComponent("§eAwarded to §6§o"+playerName));
+        tooltip.add(new TextComponent("§eMined §6§o"+uses+"§r§e blocks"));
     }
     @Override
-    public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
-        return super.onEntityItemUpdate(stack, entity);
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int p_41407_, boolean p_41408_) {
+        if(!level.isClientSide){
+            if(stack.getTag() == null){
+                // Set playerName and uses nbt if it doesn't exist, if it does exist, skip all of this.
+                stack.setTag(new CompoundTag());
+                CompoundTag nbtData = stack.getTag();
+                nbtData.putString("cupsaddons.playerName", entity.getScoreboardName());
+                nbtData.putString("cupsaddons.uses", "0");
+                stack.setTag(nbtData);
+                CupsAddons.LOGGER.info("Cup's Addons: "+entity.getScoreboardName()+" received a Expert's Pickaxe!");
+            }
+        }
+        super.inventoryTick(stack, level, entity, p_41407_, p_41408_);
     }
-
+    @Override
+    public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity entity) {
+        if(!level.isClientSide){
+            // Increase uses nbt
+            if (stack.getTag() != null) {
+                if (stack.getTag().get("cupsaddons.uses") != null) {
+                    if (stack.getTag().get("cupsaddons.uses").getAsString() != null) {
+                        int uses = Integer.parseInt(stack.getTag().get("cupsaddons.uses").getAsString());
+                        stack.getTag().remove("cupsaddons.uses");
+                        CompoundTag nbtData = stack.getTag();
+                        nbtData.putInt("cupsaddons.uses", uses + 1);
+                        entity.getMainHandItem().setTag(nbtData);
+                    }
+                }
+            }
+        }
+        return super.mineBlock(stack, level, state, pos, entity);
+    }
 
     // Animation and Model =============================================================================================
 
