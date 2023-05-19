a675f4ce3259d86a5b23c6bb1aa90ad4f7700290
Matt Borle (Cup) <mattrb@telus.net>
2023-05-19 16:57:54 -0700

    Ferro

--- src/main/java/com/mattborle/cupsaddons/config/CupsAddonsCommonConfigs.java
+++ src/main/java/com/mattborle/cupsaddons/config/CupsAddonsCommonConfigs.java
@@ -62,8 +62,8 @@ public class CupsAddonsCommonConfigs {
                 .define("Ferromagnetic Object's Active Ability (Boolean)", true);
         //PASSIVE_ABILITY_FERROMAGNETIC_OBJECT = BUILDER.comment("Enable the Ferromagnetic Object's passive ability (Default: true)")
         //        .define("Ferromagnetic Object's Passive Ability (Boolean)", true);
-        COOLDOWN_FERROMAGNETIC_OBJECT = BUILDER.comment("How strong should the Ferromagnetic Object be (Applies to most attributes including abilities) (Default: 5.0 = 5s)")
-                .defineInRange("Ferromagnetic Object Cooldown (s)", 0.0, 0.0, 86400.0);
+        COOLDOWN_FERROMAGNETIC_OBJECT = BUILDER.comment("How low the cooldown should be for the Ferromagnetic Object (Default: 5.0 = 5s)")
+                .defineInRange("Ferromagnetic Object Cooldown (s)", 5.0, 0.0, 86400.0);
         TUNE_FERROMAGNETIC_OBJECT = BUILDER.comment("How strong should the Ferromagnetic Object be (Applies to most attributes including abilities) (Default: 1.0 = 100%)")
                 .defineInRange("Ferromagnetic Object Strength (%)", 1.0, 0.0, 100.0);
         BUILDER.pop();
--- src/main/java/com/mattborle/cupsaddons/item/curios/FerromagneticObjectItem.java
+++ src/main/java/com/mattborle/cupsaddons/item/curios/FerromagneticObjectItem.java
@@ -5,16 +5,19 @@ import com.mattborle.cupsaddons.CupsAddons;
 import com.mattborle.cupsaddons.config.CupsAddonsCommonConfigs;
 import com.mattborle.cupsaddons.init.ItemRegistry;
 import com.mojang.serialization.Codec;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.core.particles.DustColorTransitionOptions;
-import net.minecraft.core.particles.ParticleType;
-import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.particles.*;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -44,6 +47,7 @@ import static com.mattborle.cupsaddons.config.CupsAddonsCommonConfigs.*;
 public class FerromagneticObjectItem extends Item implements ICurioItem {
 
     static final int RANGE = 8; // Range of effect
+    static final int COOLDOWN = (int)(COOLDOWN_FERROMAGNETIC_OBJECT.get().floatValue()*20); // Cooldown in ticks
     static final float DAMAGE = 2.0f * TUNE_FERROMAGNETIC_OBJECT.get().floatValue();
 
     public FerromagneticObjectItem(Properties properties) {
@@ -74,18 +78,22 @@ public class FerromagneticObjectItem extends Item implements ICurioItem {
     public void curioTick(SlotContext slotContext, ItemStack stack) {
         LivingEntity player = slotContext.entity();
         Level level = player.getLevel();
-
         CompoundTag tag = stack.getOrCreateTag();
-        if (tag.getBoolean("cupsaddons.has_used") && !player.isShiftKeyDown()) {
-            tag.putBoolean("cupsaddons.has_used", false);
-        } else if (!tag.getBoolean("cupsaddons.has_used") && player.isShiftKeyDown()) {
-            tag.putBoolean("cupsaddons.has_used", true);
 
-            // Do active ability if enabled
-            if(ACTIVE_ABILITY_FERROMAGNETIC_OBJECT.get()) {
+        // Do active ability if enabled
+        if(ACTIVE_ABILITY_FERROMAGNETIC_OBJECT.get()) {
 
-                if (level instanceof ServerLevel serverLevel) {
-                    // On the server
+            if (level instanceof ServerLevel serverLevel) {
+                // On the server
+                // If cooldown has not finished
+                if (tag.getInt("cupsaddons.cooldown") >= 0)
+                { // decrement cooldown nbt
+                    tag.putInt("cupsaddons.cooldown", tag.getInt("cupsaddons.cooldown") - 1);
+                } // If cooldown has finished
+                else if (tag.getInt("cupsaddons.cooldown") < 1 && player.isShiftKeyDown())
+                {
+                    tag.putInt("cupsaddons.cooldown", COOLDOWN); // Put fresh cooldown in ticks
+
                     // Play magnet sound
                     serverLevel.playSound(null, player.getX(), player.getY(), player.getZ(), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("cupsaddons:magnetize")), SoundSource.PLAYERS, 1.2f, 0.9f + player.getRandom().nextFloat() * 0.1f);
                     // Find nearby entities
@@ -109,17 +117,19 @@ public class FerromagneticObjectItem extends Item implements ICurioItem {
                                     if (entityHead.getItem() == Items.IRON_HELMET || entityHead.getItem() == Items.CHAINMAIL_HELMET ||
                                             entityChest.getItem() == Items.IRON_CHESTPLATE || entityChest.getItem() == Items.CHAINMAIL_CHESTPLATE ||
                                             entityLegs.getItem() == Items.IRON_LEGGINGS || entityLegs.getItem() == Items.CHAINMAIL_LEGGINGS ||
-                                            entityFeet.getItem() == Items.IRON_BOOTS || entityFeet.getItem() == Items.CHAINMAIL_BOOTS) {
+                                            entityFeet.getItem() == Items.IRON_BOOTS || entityFeet.getItem() == Items.CHAINMAIL_BOOTS)
+                                    {
                                         // Repel entities that fail any of the above tests
                                         for (int strength = 30; strength > 0; strength--) {
                                             entity.push(player);
                                         }
                                         // Briefly give entity glowing effect
-                                        entity.addEffect(new MobEffectInstance(MobEffects.GLOWING, 3, 1, (false), (false)));
-                                        entity.playSound(ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("cupsaddons:magnetize")), 1.5f, 1f);
+                                        entity.addEffect(new MobEffectInstance(MobEffects.GLOWING, 8, 1, (false), (false)));
+                                        entity.playSound(SoundEvents.PLAYER_ATTACK_KNOCKBACK, 0.8f, 1f);
                                         // Spawn particles around entity
                                         // sendParticles to level (ParticleType,x,y,z,numParticles,dx,dy,dz,speed)
-                                        serverLevel.sendParticles(ParticleTypes.CRIT, entity.getX(),entity.getY()+1,entity.getZ(),8,0,1,0,0.5);
+                                        serverLevel.sendParticles(ParticleTypes.GLOW, entity.getX(),entity.getY()+1,entity.getZ(),4,0,1,0,0.1);
+                                        serverLevel.sendParticles(ParticleTypes.SMOKE, entity.getX(),entity.getY()+1,entity.getZ(),10,0,1,0,0.1);
                                     }
                                 } catch (Exception e) {
                                     CupsAddons.LOGGER.error(e.getStackTrace().toString());
@@ -133,7 +143,7 @@ public class FerromagneticObjectItem extends Item implements ICurioItem {
                     int numParticles = 30;
                     for(int i = 0; i < numParticles; i++) {
                         // sendParticles to level (ParticleType,x,y,z,numParticles,dx,dy,dz,speed)
-                        serverLevel.sendParticles(ParticleTypes.POOF, player.getX()+((RANGE/2)*Math.cos(angle)),player.getY()+0.2,player.getZ()+((RANGE/2)*Math.sin(angle)),0,0.1,0.1,0.1,0);
+                        serverLevel.sendParticles(ParticleTypes.GLOW, player.getX()+((RANGE/2)*Math.cos(angle)),player.getY()+0.5,player.getZ()+((RANGE/2)*Math.sin(angle)),0,0,0,0,0);
                         angle += (Math.PI*2)/numParticles;
                     }
                 }
