1d1b399055a0b1c4f19be23d4f9c1216750168d9
Matt Borle (Cup) <mattrb@telus.net>
2023-08-06 22:30:43 -0700

    Tree generation disabled
    trees generate too commonly, disabled until time to find rarity parameter

--- src/main/java/com/mattborle/cupsaddons/world/feature/ModConfiguredFeatures.java
+++ src/main/java/com/mattborle/cupsaddons/world/feature/ModConfiguredFeatures.java
@@ -3,14 +3,20 @@ package com.mattborle.cupsaddons.world.feature;
 import com.mattborle.cupsaddons.init.BlockRegistry;
 import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
+import java.util.List;
 
 public class ModConfiguredFeatures {
 
@@ -19,7 +25,7 @@ public class ModConfiguredFeatures {
     public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> GOLDEN_OAK_TREE =
             FeatureUtils.register("golden_oak", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(BlockRegistry.GOLDEN_OAK_LOG.get()),
-                    new StraightTrunkPlacer(2,1,1),
+                    new StraightTrunkPlacer(3,1,1),
                     BlockStateProvider.simple(BlockRegistry.METALLIC_OAK_LEAVES.get()),
                     new BlobFoliagePlacer(ConstantInt.of(2),ConstantInt.of(0), 3),
                     new TwoLayersFeatureSize(1, 0, 1)
@@ -27,7 +33,7 @@ public class ModConfiguredFeatures {
     public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> IRON_OAK_TREE =
             FeatureUtils.register("iron_oak", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(BlockRegistry.IRON_OAK_LOG.get()),
-                    new StraightTrunkPlacer(2,1,1),
+                    new StraightTrunkPlacer(3,1,1),
                     BlockStateProvider.simple(BlockRegistry.METALLIC_OAK_LEAVES.get()),
                     new BlobFoliagePlacer(ConstantInt.of(2),ConstantInt.of(0), 3),
                     new TwoLayersFeatureSize(1, 0, 1)
@@ -35,7 +41,7 @@ public class ModConfiguredFeatures {
     public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> COPPER_OAK_TREE =
             FeatureUtils.register("copper_oak", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(BlockRegistry.COPPER_OAK_LOG.get()),
-                    new StraightTrunkPlacer(2,1,1),
+                    new StraightTrunkPlacer(3,1,1),
                     BlockStateProvider.simple(BlockRegistry.METALLIC_OAK_LEAVES.get()),
                     new BlobFoliagePlacer(ConstantInt.of(2),ConstantInt.of(0), 3),
                     new TwoLayersFeatureSize(1, 0, 1)
@@ -43,7 +49,7 @@ public class ModConfiguredFeatures {
     public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> ZINC_OAK_TREE =
             FeatureUtils.register("zinc_oak", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(BlockRegistry.ZINC_OAK_LOG.get()),
-                    new StraightTrunkPlacer(2,1,1),
+                    new StraightTrunkPlacer(3,1,1),
                     BlockStateProvider.simple(BlockRegistry.METALLIC_OAK_LEAVES.get()),
                     new BlobFoliagePlacer(ConstantInt.of(2),ConstantInt.of(0), 3),
                     new TwoLayersFeatureSize(1, 0, 1)
@@ -51,9 +57,46 @@ public class ModConfiguredFeatures {
     public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> RICH_OAK_TREE =
             FeatureUtils.register("rich_oak", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                     BlockStateProvider.simple(BlockRegistry.RICH_OAK_LOG.get()),
-                    new StraightTrunkPlacer(2,1,1),
+                    new StraightTrunkPlacer(3,1,1),
                     BlockStateProvider.simple(BlockRegistry.METALLIC_OAK_LEAVES.get()),
                     new BlobFoliagePlacer(ConstantInt.of(2),ConstantInt.of(0), 3),
                     new TwoLayersFeatureSize(1, 0, 1)
             ).build());
+
+
+    // ore tree spawners ===============================================================================================
+    public static final Holder<PlacedFeature> GOLDEN_OAK_CHECKED = PlacementUtils.register("golden_oak_checked", GOLDEN_OAK_TREE,
+            PlacementUtils.filteredByBlockSurvival(BlockRegistry.GOLDEN_OAK_SAPLING.get()));
+    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> GOLDEN_OAK_SPAWN =
+            FeatureUtils.register("golden_oak_spawn", Feature.RANDOM_SELECTOR,
+                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(GOLDEN_OAK_CHECKED,
+                            0.005F)), GOLDEN_OAK_CHECKED));
+
+    public static final Holder<PlacedFeature> IRON_OAK_CHECKED = PlacementUtils.register("iron_oak_checked", IRON_OAK_TREE,
+            PlacementUtils.filteredByBlockSurvival(BlockRegistry.IRON_OAK_SAPLING.get()));
+    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> IRON_OAK_SPAWN =
+            FeatureUtils.register("iron_oak_spawn", Feature.RANDOM_SELECTOR,
+                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(IRON_OAK_CHECKED,
+                            0.01F)), IRON_OAK_CHECKED));
+
+    public static final Holder<PlacedFeature> COPPER_OAK_CHECKED = PlacementUtils.register("copper_oak_checked", COPPER_OAK_TREE,
+            PlacementUtils.filteredByBlockSurvival(BlockRegistry.COPPER_OAK_SAPLING.get()));
+    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> COPPER_OAK_SPAWN =
+            FeatureUtils.register("copper_oak_spawn", Feature.RANDOM_SELECTOR,
+                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(COPPER_OAK_CHECKED,
+                            0.01F)), COPPER_OAK_CHECKED));
+
+    public static final Holder<PlacedFeature> ZINC_OAK_CHECKED = PlacementUtils.register("zinc_oak_checked", ZINC_OAK_TREE,
+            PlacementUtils.filteredByBlockSurvival(BlockRegistry.ZINC_OAK_SAPLING.get()));
+    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> ZINC_OAK_SPAWN =
+            FeatureUtils.register("zinc_oak_spawn", Feature.RANDOM_SELECTOR,
+                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(ZINC_OAK_CHECKED,
+                            0.01F)), ZINC_OAK_CHECKED));
+
+    public static final Holder<PlacedFeature> RICH_OAK_CHECKED = PlacementUtils.register("rich_oak_checked", RICH_OAK_TREE,
+            PlacementUtils.filteredByBlockSurvival(BlockRegistry.RICH_OAK_SAPLING.get()));
+    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> RICH_OAK_SPAWN =
+            FeatureUtils.register("rich_oak_spawn", Feature.RANDOM_SELECTOR,
+                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(RICH_OAK_CHECKED,
+                            0.001F)), RICH_OAK_CHECKED));
 }
---
+++ src/main/java/com/mattborle/cupsaddons/world/feature/ModPlacedFeatures.java
@@ -0,0 +1,25 @@
+package com.mattborle.cupsaddons.world.feature;
+
+import net.minecraft.core.Holder;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
+public class ModPlacedFeatures {
+    public static final Holder<PlacedFeature> GOLDEN_OAK_PLACED = PlacementUtils.register("golden_oak_placed",
+            ModConfiguredFeatures.GOLDEN_OAK_SPAWN, VegetationPlacements.treePlacement(
+                    PlacementUtils.countExtra(1, 0.1f, 1)));
+    public static final Holder<PlacedFeature> IRON_OAK_PLACED = PlacementUtils.register("iron_oak_placed",
+            ModConfiguredFeatures.IRON_OAK_SPAWN, VegetationPlacements.treePlacement(
+                    PlacementUtils.countExtra(2, 0.1f, 1)));
+    public static final Holder<PlacedFeature> COPPER_OAK_PLACED = PlacementUtils.register("copper_oak_placed",
+            ModConfiguredFeatures.COPPER_OAK_SPAWN, VegetationPlacements.treePlacement(
+                    PlacementUtils.countExtra(2, 0.1f, 1)));
+    public static final Holder<PlacedFeature> ZINC_OAK_PLACED = PlacementUtils.register("zinc_oak_placed",
+            ModConfiguredFeatures.ZINC_OAK_SPAWN, VegetationPlacements.treePlacement(
+                    PlacementUtils.countExtra(2, 0.1f, 1)));
+    public static final Holder<PlacedFeature> RICH_OAK_PLACED = PlacementUtils.register("rich_oak_placed",
+            ModConfiguredFeatures.RICH_OAK_SPAWN, VegetationPlacements.treePlacement(
+                    PlacementUtils.countExtra(1, 0.1f, 1)));
+
+}
