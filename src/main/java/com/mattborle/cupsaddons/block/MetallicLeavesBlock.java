6b06e4171ae6e6e8a06d45bb2c2951fb2e031423
Matt Borle (Cup) <mattrb@telus.net>
2023-08-09 17:49:51 -0700

    More polish

---
+++ src/main/java/com/mattborle/cupsaddons/block/MetallicLeavesBlock.java
@@ -0,0 +1,23 @@
+package com.mattborle.cupsaddons.block;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.LeavesBlock;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class MetallicLeavesBlock  extends LeavesBlock {
+
+    public MetallicLeavesBlock(Properties properties) { super(properties); }
+
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable BlockGetter blockGetter, List<Component> tooltip, TooltipFlag tooltipFlag) {
+        tooltip.add(new TextComponent("\n§72% chance to drop an Ore Tree Sapling\n"));
+        super.appendHoverText(itemStack, blockGetter, tooltip, tooltipFlag);
+    }
+}
--- src/main/java/com/mattborle/cupsaddons/block/RichLeavesBlock.java
+++ src/main/java/com/mattborle/cupsaddons/block/RichLeavesBlock.java
@@ -2,20 +2,42 @@ package com.mattborle.cupsaddons.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Random;
 
 public class RichLeavesBlock extends LeavesBlock {
+
     public RichLeavesBlock(Properties properties) {
         super(properties);
     }
 
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable BlockGetter blockGetter, List<Component> tooltip, TooltipFlag tooltipFlag) {
+        tooltip.add(new TextComponent("\n§b1% chance to drop a Rich Ore Tree Sapling\n"));
+        super.appendHoverText(itemStack, blockGetter, tooltip, tooltipFlag);
+    }
+
+
     @Override
     public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, Random random) {
+        serverLevel.sendParticles(ParticleTypes.GLOW, blockPos.getX()+0.5, blockPos.getY()+0.5, blockPos.getZ()+0.5, 2, 0.25, 0.25, 0.25, 1);
+        super.tick(blockState, serverLevel, blockPos, random);
+    }
+
+    @Override
+    public void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, Random random) {
         serverLevel.sendParticles(ParticleTypes.GLOW, blockPos.getX()+0.5, blockPos.getY()+0.5, blockPos.getZ()+0.5, 1, 0.25, 0.25, 0.25, 1);
         super.tick(blockState, serverLevel, blockPos, random);
     }
