c015f504f8491018e6f2338d9baf4dded43a1883
Matt Borle (Cup) <mattrb@telus.net>
2024-05-08 23:55:36 -0700

    Update CompactedOreBlock.java

--- src/main/java/com/mattborle/cupsaddons/block/CompactedOreBlock.java
+++ src/main/java/com/mattborle/cupsaddons/block/CompactedOreBlock.java
@@ -8,7 +8,7 @@ public class CompactedOreBlock extends OreBlock {
     public CompactedOreBlock(Properties properties) {
         super(properties
                 .requiresCorrectToolForDrops()
-                .destroyTime(5000)          // TODO: make these values configurable
+                .destroyTime(1000000)          // TODO: make these values configurable
                 .sound(SoundType.DEEPSLATE_BRICKS)  // sounds like deepslate bricks
                 .strength(10.0f));           // somewhat resistant to explosions
     }
